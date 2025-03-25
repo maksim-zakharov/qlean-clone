a111060603de3c2e410bb3d4df22fe23ebde5161
Maksim Zakharov <max89701@gmail.com>
2025-03-25 21:15:28 +0300

    vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,22 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         tailwindcss(),
         react()
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                // Добавляем хеш к именам файлов
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: `assets/[name].[hash].[ext]`
+            }
+        }
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
