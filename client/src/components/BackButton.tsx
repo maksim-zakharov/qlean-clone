beb742d6066815ac3cb462965e847eecddadab45
Maksim Zakharov <max89701@gmail.com>
2025-03-26 17:48:12 +0300

    vite

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -4,15 +4,17 @@ import {useNavigate} from "react-router-dom";
 import {FC, useEffect} from "react";
 import {useTelegram} from "../hooks/useTelegram.ts";
 
-export const BackButton: FC<{ onBack?: () => void }> = ({onBack}) => {
+export const BackButton: FC<any> = () => {
     const navigate = useNavigate()
     const {backButton, isLoading, error} = useTelegram();
 
     const showTgBackButton = Boolean(backButton && !isLoading && !error);
 
+    const onBack = () => navigate('/')
+
     useEffect(() => {
         if (backButton && showTgBackButton) {
-            // Telegram.WebApp.onEvent('backButtonClicked', onBack);
+            Telegram.WebApp.onEvent('backButtonClicked', onBack);
             backButton.show();
         }
     }, [backButton, showTgBackButton]);
@@ -24,7 +26,7 @@ export const BackButton: FC<{ onBack?: () => void }> = ({onBack}) => {
     return <Button
         variant="ghost"
         className="p-2 -ml-2 text-tg-theme-button-color hover:bg-transparent"
-        onClick={() => navigate('/')}
+        onClick={onBack}
     >
         <ChevronLeft className="h-5 w-5"/>
     </Button>
