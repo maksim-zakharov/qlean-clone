40e7f112573d525a8d009fee451c040f050f1df6
Maksim Zakharov <max89701@gmail.com>
2025-03-26 17:36:20 +0300

    vite

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './index.css'  // Импорт Tailwind
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>
 )
