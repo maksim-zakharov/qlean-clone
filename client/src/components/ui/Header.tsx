dee56d22e5284db5039badab72e5aa9325ebe956
Maksim Zakharov <max89701@gmail.com>
2025-03-26 23:13:12 +0300

    vite

--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 import {FC} from "react";
 
 export const Header: FC<any> = ({isWebApp, children}: any) =>
-    <header className={`fixed top-0 left-0 right-0 ${isWebApp ? "pt-[env(safe-area-inset-top)]" : "pt-2"} z-10 bg-tg-theme-secondary-bg-color flex-none px-4 pb-2`}>
+    <header className={`fixed top-0 left-0 right-0 border-b ${isWebApp ? "pt-[env(safe-area-inset-top)]" : "pt-2"} z-10 bg-tg-theme-secondary-bg-color flex-none px-4 pb-2`}>
 
             {children}
 </header>
