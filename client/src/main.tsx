31b174ab02a57b5824ec0b4d67cbd0c705890e49
Maksim Zakharov <max89701@gmail.com>
2025-03-26 16:16:28 +0300

    vite

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,8 @@ import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store.ts";
 
+Telegram.WebApp?.ready?.();
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <HashRouter>
