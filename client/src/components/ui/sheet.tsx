a4e7fa91be8e4077b55685fb66a29725e81d5fac
Maksim Zakharov <max89701@gmail.com>
2025-04-15 23:03:19 +0300

    прозрачность менюхи

--- client/src/components/ui/sheet.tsx
+++ client/src/components/ui/sheet.tsx
@@ -73,8 +73,8 @@ function SheetContent({
                 )}
                 {...props}
             >
-                {bottomOffset}
                 {children}
+                {bottomOffset}
                 {extra && <div className="absolute top-4 right-4">{extra}</div>}
                 {/*<SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">*/}
                 {/*  <XIcon className="size-4" />*/}
