e42a522b9db7d2fdf111fe80e4cb7cebfb1b5c43
Maksim Zakharov <max89701@gmail.com>
2025-04-15 22:28:50 +0300

    прозрачность менюхи

--- client/src/components/ui/sheet.tsx
+++ client/src/components/ui/sheet.tsx
@@ -3,6 +3,7 @@ import {ReactNode} from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 
 import {cn} from "@/lib/utils"
+import {useTelegram} from "../../hooks/useTelegram.ts";
 
 function Sheet({...props}: React.ComponentProps<typeof SheetPrimitive.Root>) {
     return <SheetPrimitive.Root data-slot="sheet" {...props} />
@@ -51,13 +52,14 @@ function SheetContent({
                       }: React.ComponentProps<typeof SheetPrimitive.Content> & {
     side?: "top" | "right" | "bottom" | "left"
 } & { extra?: ReactNode }) {
+    const {bottomOffset} = useTelegram();
     return (
         <SheetPortal>
             <SheetOverlay/>
             <SheetPrimitive.Content
                 data-slot="sheet-content"
                 className={cn(
-                    "pb-[calc(16px+env(safe-area-inset-bottom))] p-4 rounded-t-2xl root-bg-color data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-300",
+                    `mb-[${bottomOffset}px] pb-[calc(16px+env(safe-area-inset-bottom))] p-4 rounded-t-2xl root-bg-color data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-300`,
                     side === "right" &&
                     "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
                     side === "left" &&
@@ -70,6 +72,7 @@ function SheetContent({
                 )}
                 {...props}
             >
+                {bottomOffset}
                 {children}
                 {extra && <div className="absolute top-4 right-4">{extra}</div>}
                 {/*<SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">*/}
