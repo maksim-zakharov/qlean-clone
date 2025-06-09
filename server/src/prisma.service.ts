74610979bbe7030be449e763c85fe9ca289ddd5b
Maksim Zakharov <max89701@gmail.com>
2025-06-09 14:58:59 +0300

    baseService deletedAt

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -10,6 +10,43 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     /***********************************/
 
     this.$use(async (params, next) => {
+      if (params.model == 'BaseService') {
+        if (params.action === 'findUnique' || params.action === 'findFirst') {
+          // Change to findFirst - you cannot filter
+          // by anything except ID / unique with findUnique()
+          params.action = 'findFirst';
+          // Add 'deleted' filter
+          // ID filter maintained
+          params.args.where['deletedAt'] = null;
+        }
+        if (
+          params.action === 'findFirstOrThrow' ||
+          params.action === 'findUniqueOrThrow'
+        ) {
+          if (params.args.where) {
+            if (params.args.where.deletedAt == undefined) {
+              // Exclude deleted records if they have not been explicitly requested
+              params.args.where['deletedAt'] = null;
+            }
+          } else {
+            params.args['where'] = { deletedAt: null };
+          }
+        }
+        if (params.action === 'findMany') {
+          // Find many queries
+          if (params.args.where) {
+            if (params.args.where.deletedAt == undefined) {
+              params.args.where['deletedAt'] = null;
+            }
+          } else {
+            params.args['where'] = { deletedAt: null };
+          }
+        }
+      }
+      return next(params);
+    });
+
+    this.$use(async (params, next) => {
       // Check incoming query type
       if (params.model == 'BaseService') {
         if (params.action == 'delete') {
